b6310438965ae560fea51713631e9c5c258e6e90
Tyler Ayers <tyayers@microsoft.com>
2018-11-19 12:21:51 +0100

    first version

---
+++ AzureAlertHubFunctions/AzureAlertFunction.cs
@@ -0,0 +1,28 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace AzureAlertHubFunctions
+{
+    public static class AzureAlertFunction
+    {
+        [FunctionName("AzureAlertFunction")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+
+            log.LogInformation(requestBody);
+            return (ActionResult)new OkObjectResult(requestBody);
+        }
+    }
+}
